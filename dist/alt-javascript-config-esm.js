11e39d5ce334c278590313eb81d69adc2ea1b839
craig <craigparra@gmail>
2022-03-10 16:57:02 +1000

    window location specific config, better domain/IP syntax

--- dist/alt-javascript-config-esm.js
+++ dist/alt-javascript-config-esm.js
@@ -319,16 +319,19 @@ class WindowLocationConfig extends DelegatingConfig {
 
   // eslint-disable-next-line class-methods-use-this
   has(path) {
-    return this.config.has(`${window.location.origin}${window.location.pathname}.${path}`)
+    const location = `${window.location.origin}${window.location.pathname}`.replaceAll('.','+');
+
+    return this.config.has(`${location}.${path}`)
         || this.config.has(path);
   }
 
   get(path, defaultValue) {
+    const location = `${window.location.origin}${window.location.pathname}`.replaceAll('.','+');
     if ((typeof defaultValue !== 'undefined') && this.has(path) === false) {
       return defaultValue;
     }
-    if (this.config.has(`${window.location.origin}${window.location.pathname}.${path}`)) {
-      return this.config.get(`${window.location.origin}${window.location.pathname}.${path}`);
+    if (this.config.has(`${location}.${path}`)) {
+      return this.config.get(`${location}.${path}`);
     }
     return this.config.get(path);
   }
